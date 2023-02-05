88fd37e62c937c4b809169da39bea7f7d375d729
gabrielsousaf <gs294860@gmail.com>
2023-02-04 21:53:31 -0300

    Update Responsive

--- src/js/script.js
+++ src/js/script.js
@@ -5,15 +5,56 @@ let typed = new Typed(".typing", {
     loop: true
 })
 
-AOS.init();
 
-AOS.init({
-  offset: 120, 
-  delay: 0,
-  duration: 700, 
-  easing: 'ease', 
-  once: false, 
-  mirror: false, 
-  anchorPlacement: 'top-bottom', 
+/* ========================= Toggle Style Switcher ========================= */
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("click", () => {
+  document.querySelector(".style-switcher").classList.toggle("open");
 
-});
+})
+
+
+// Hide style - switcher on scroll //
+window.addEventListener("scroll",() =>{
+  if(document.querySelector(".style-switcher").classList.contains("open")) {
+
+    document.querySelector(".style-switcher").classList.remove("open");
+  }
+})
+
+
+// TEMA CORES
+const alternateStyles = document.querySelectorAll(".alternate-style")
+function setActiveStyle(color) {
+    alternateStyles.forEach((style) => {
+        if(color === style.getAttribute("title")) {
+            style.removeAttribute("disabled")
+        }
+        else {
+            style.setAttribute("disabled", "true")
+        }
+    })
+}
+
+
+// DARK MODE 
+const dayNight = document.querySelector(".day-night");
+window.addEventListener("load", () => {
+  if(document.body.classList.contains("dark")) {
+
+    dayNight.querySelector("i").classList.add("fa-sun")
+  }
+  else{
+    dayNight.querySelector("i").classList.add("fa-moon")
+  }
+  
+})
+
+
+
+document.querySelectorAll('.nav li a').forEach(el => {
+  el.onclick = () => {
+    document.querySelectorAll('.nav li a').forEach(el => el.classList.remove('active'));
+    el.classList.add('active');
+  }
+})
